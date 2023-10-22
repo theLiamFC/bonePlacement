32a4090eaec5bb04b86a74c118c0a32a238e460b
theLiamFC <147665958+theLiamFC@users.noreply.github.com>
2023-10-22 17:07:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,35 +5,76 @@ import Servo
 import Giotto
 import ArmDrive
 import BlueTooth as bt
+import myWifi
+import mqtt
+import json
+
+myWifi.connect(myWifi.TUFTS)
+
+vacuum = Pin(21, Pin.OUT)
+vacuum.on()
+setLow = Servo.ContinuousServo(27)
+setLow.set_speed(-40)
+
+message = ""
 
 # class to drive the car, assumes 360 degree servos
 class TankDrive:
     def __init__(self,leftPin,rightPin):
         self.leftM = Servo.ContinuousServo(pin_number=leftPin)
         self.rightM = Servo.ContinuousServo(pin_number=rightPin)
-        self.pace = 1
 
     def move(self,joyY,joyX):
-        leftMotor = (joyY - joyX) * self.pace
-        rightMotor = -1 * (joyY + joyX) * self.pace
+        leftMotor = 100 * (joyY - joyX)
+        rightMotor = -100 * (joyY + joyX)
         
-        leftM.set_speed(leftMotor)
-        rightM.set_speed(rightMotor)
+        self.leftM.set_speed(int(leftMotor-17))
+        self.rightM.set_speed(int(rightMotor-17))
+    
+    #def surgeryMove(self,
 
+def gotMessage(topic, msg):
+    
+    #print("recieved a message")
+    #print((topic.decode(), msg.decode()))
+    
+    info = msg.decode()
+    message = info.split(", ")
+    inputDict = json.loads(info)
+    print(inputDict)
+    
+    if inputDict['vac']:
+        vacuum.on()
+    else:
+        vacuum.off()
+        
+    theCar.move(inputDict['leftJoyY'],inputDict['leftJoyX'])
+    
+        
+theCar= TankDrive(26,27)
+theArm = ArmDrive.ArmDrive(pinA=12,pinB=13,pinZ=14,pinR=15,lenA=100,lenB=100,minX=10)
 
-theArm = ArmDrive.ArmDrive(pinA=12,pinB=13,pinZ=14,lenA=60,lenB=60,minX=10)
+try:
+    fred = mqtt.MQTTClient('MyPico', 'broker.hivemq.com', keepalive=1000)
+    print('Connected to MQTT')
+    fred.connect()
+    fred.set_callback(gotMessage)
+except OSError as e:
+    print('Failed')
+    
+fred.subscribe("theVacuum")
 
-for i in range(20):
-    print("trying bluetooth")
-    try:   
-        L = bt.Listen('aMilliaMilliaMilli')
-        if L.connect_up(on_rx):
-            time.sleep(5)
-            L.write('heyo')
-            time.sleep(5)
-    except Exception as e:
-        print(e)
-
+try:
+    while True:
+        fred.check_msg()
+        time.sleep(0.05)
+        #debug
+        #testInput = input("Enter angles: A B Z\n").split(" ")
+        #theArm.setAngles(int(testInput[0]),int(testInput[1]),int(testInput[2]))
+        #testInput = input("Enter position: X Y Z R\n").split(" ")
+        #print(theArm.move(int(testInput[0]),int(testInput[1]),int(testInput[2])))
+        #theArm.armR.set_speed(int(testInput[3]))
+except Exception as e:
+    print(e)
 finally:
-    print('closing up')
-    L.disconnect()
+    fred.disconnect()
